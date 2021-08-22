b1df8e3c44304c09bbff43bdb48f917a166d1a9e
Arslanoov <rasularslanoov@gmail.com>
2021-08-22 23:18:55 +0500

    feat(algo): Linked list traversal

--- src/algo/sort/bubble/bubbleSortPure.spec.js
+++ src/algo/sort/bubble/bubbleSortPure.spec.js
@@ -1,6 +1,6 @@
 import { bubbleSort } from "./bubbleSortPure"
 
-describe("Algo: Bubble Sort", () => {
+describe("Algo: Bubble Sort with Vanilla JS", () => {
   it("works", () => {
     expect(bubbleSort([2, 4, 7, 1])).toStrictEqual([1, 2, 4, 7])
     expect(bubbleSort([1, 8, 9, 3])).toStrictEqual([1, 3, 8, 9])
