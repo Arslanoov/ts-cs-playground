73e111cd9251bc92248f42ce2b4107531eb59a89
Arslanoov <rasularslanoov@gmail.com>
2021-08-23 22:41:51 +0500

    feat(algo): Fibonacci number

---
+++ src/algo/math/fibonacci/fibonacci.ts
@@ -0,0 +1,46 @@
+// O(n)
+// fib(1) -> [1]
+// fib(2) -> [1, 1] (1 + 0)
+// fib(3) -> [1, 1, 2] (1 + 1)
+// fib(4) -> [1, 1, 2, 3] (1 + 2)
+export const fib = (pos: number): number[] => {
+  const path: number[] = []
+  let current: number = 1
+  let prev: number = 0
+  let prevCurrent: number = 0
+
+  while (pos > 0) {
+    prevCurrent = current
+    current = current + prev
+    prev = prevCurrent
+    path.push(prevCurrent)
+    pos--
+  }
+
+  return path
+}
+
+export const fibValue = (pos: number): number => {
+  let current: number = 1
+  let prev: number = 0
+  let prevCurrent: number = 0
+
+  while (pos > 0) {
+    prevCurrent = current
+    current = current + prev
+    prev = prevCurrent
+    pos--
+  }
+
+  return prev
+}
+
+export const fibRecursionValue = (pos: number, path: {
+  [key: number]: number
+} = {}): number => {
+  if (path[pos]) return path[pos]
+  if (pos < 1) return 0
+  if (pos === 1) return 1
+
+  return path[pos] = fibRecursionValue(pos - 1, path) + fibRecursionValue(pos - 2, path)
+}
