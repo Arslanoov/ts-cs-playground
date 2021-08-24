acd95ff016ca469d94d0edb7c887662ff94d0b56
Arslanoov <rasularslanoov@gmail.com>
2021-08-24 20:48:12 +0500

    feat(algo): Insertion sort

---
+++ src/algo/sort/insertion/insertionSortPure.js
@@ -0,0 +1,12 @@
+// O(n^2)
+export const insertionSort = (input) => {
+  const arr = [...input]
+  for (let i = 1; i < input.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j - 1] > arr[j]) {
+        [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
+      }
+    }
+  }
+  return arr
+}
