3f526a96a05ae2838971d23baddfceea6f6aa463
Arslanoov <rasularslanoov@gmail.com>
2021-08-27 00:37:02 +0500

    feat(algo): Jump search

---
+++ src/algo/search/jump/jumpSearchPure.js
@@ -0,0 +1,21 @@
+// O(sqrt(n))
+export const jumpSearch = (arr, item) => {
+  const size = arr.length
+  const jumpSize = Math.floor(Math.sqrt(size))
+
+  let start = 0
+  let end = jumpSize
+  while (item > arr[Math.min(end, size) - 1]) {
+    start = end
+    end += jumpSize
+    if (start > size) return -1
+  }
+
+  let current = start
+  while (current < Math.min(end, size)) {
+    if (arr[current] === item) return current
+    current++
+  }
+
+  return -1
+}
