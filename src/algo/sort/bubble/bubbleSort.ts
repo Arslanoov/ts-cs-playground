41d47263d4bb445338d8bc1c039de588c6bcb447
Arslanoov <rasularslanoov@gmail.com>
2021-10-13 22:49:34 +0500

    fix(sort): Fixed bubble sort

--- src/algo/sort/bubble/bubbleSort.ts
+++ src/algo/sort/bubble/bubbleSort.ts
@@ -2,7 +2,7 @@ export const bubbleSort = (input: number[]): number[] => {
   const arr = [...input]
 
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
+    for (let j = 0; j < arr.length - i; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
       }
