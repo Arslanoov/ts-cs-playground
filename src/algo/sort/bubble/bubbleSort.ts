71c8cbfa3759b02151a33230b2a4c7c4aed5100d
Arslanoov <rasularslanoov@gmail.com>
2021-10-14 17:01:28 +0500

    fix(sort): Optimized bubble sort

--- src/algo/sort/bubble/bubbleSort.ts
+++ src/algo/sort/bubble/bubbleSort.ts
@@ -1,12 +1,17 @@
 export const bubbleSort = (input: number[]): number[] => {
   const arr = [...input]
+  let notSwapped: boolean = false
 
   for (let i = 0; i < arr.length; i++) {
+    notSwapped = true
     for (let j = 0; j < arr.length - i; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        notSwapped = false
       }
     }
+
+    if (notSwapped) break
   }
 
   return arr
