7f8f370da39019aa819203efa59c5874587ae164
Arslanoov <rasularslanoov@gmail.com>
2021-10-23 17:54:10 +0500

    feat(algo): DFS Post order, in order

--- src/algo/tree/dfs/depthFirstSearch.spec.ts
+++ src/algo/tree/dfs/depthFirstSearch.spec.ts
@@ -1,5 +1,5 @@
 import { BinarySearchTree } from "../../../data-structures/tree/binary-search/binarySearchTree"
-import { depthFirstSearchPreOrder } from "./depthFirstSearch"
+import { depthFirstSearchPreOrder, depthFirstSearchPostOrder, depthFirstSearchInOrder } from "./depthFirstSearch"
 
 describe("Depth first search", () => {
   test("pre order", () => {
@@ -13,5 +13,31 @@ describe("Depth first search", () => {
     tree.insert(20)
 
     expect(depthFirstSearchPreOrder(tree.root)).toStrictEqual([10, 6, 3, 8, 15, 20])
+  })
+
+  test("post order", () => {
+    const tree = new BinarySearchTree()
+
+    tree.insert(10)
+    tree.insert(6)
+    tree.insert(15)
+    tree.insert(3)
+    tree.insert(8)
+    tree.insert(20)
+
+    expect(depthFirstSearchPostOrder(tree.root)).toStrictEqual([3, 8, 6, 20, 15, 10])
+  })
+
+  test("in order", () => {
+    const tree = new BinarySearchTree()
+
+    tree.insert(10)
+    tree.insert(6)
+    tree.insert(15)
+    tree.insert(3)
+    tree.insert(8)
+    tree.insert(20)
+
+    expect(depthFirstSearchInOrder(tree.root)).toStrictEqual([3, 6, 8, 10, 15, 20])
   })
 })
--- src/algo/tree/dfs/depthFirstSearch.ts
+++ src/algo/tree/dfs/depthFirstSearch.ts
@@ -1,5 +1,8 @@
 import { BSTNode } from "../../../data-structures/tree/binary-search/binarySearchTree"
 
+/**
+ * Diff implementation
+ */
 export const depthFirstSearchPreOrder = (node: BSTNode) => {
   let items: number[] = []
 
@@ -12,6 +15,46 @@ export const depthFirstSearchPreOrder = (node: BSTNode) => {
   if (node.right) {
     items = items.concat(depthFirstSearchPreOrder(node.right))
   }
+
+  return items
+}
+
+export const depthFirstSearchPostOrder = (node: BSTNode) => {
+  let items: number[] = []
+
+  const traverse = (node: BSTNode) => {
+    if (node.left) {
+      traverse(node.left)
+    }
+
+    if (node.right) {
+      traverse(node.right)
+    }
+
+    items.push(node.value)
+  }
+
+  traverse(node)
+
+  return items
+}
+
+export const depthFirstSearchInOrder = (node: BSTNode) => {
+  let items: number[] = []
+
+  const traverse = (node: BSTNode) => {
+    if (node.left) {
+      traverse(node.left)
+    }
+
+    items.push(node.value)
+
+    if (node.right) {
+      traverse(node.right)
+    }
+  }
+
+  traverse(node)
 
   return items
 }
