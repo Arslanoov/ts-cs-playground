9c0ee879c85893d3cdca3aaa6f72f430b94f76e0
Arslanoov <rasularslanoov@gmail.com>
2021-10-28 16:57:38 +0500

    feat(hash-table): Keys and values

--- src/data-structures/hash-table/hashTable.ts
+++ src/data-structures/hash-table/hashTable.ts
@@ -5,6 +5,8 @@ export const DEFAULT_SIZE = 17
 export interface HashTableInterface<T> {
   set(key: string, value: T): void
   get(key: string): T | null
+  keys(): string[]
+  values(): T[]
 }
 
 interface KeyValue<T> {
@@ -19,6 +21,9 @@ export class HashTable<T> implements HashTableInterface<T> {
     this.map = new Array(size).fill(null).map(() => [])
   }
 
+  /**
+   * Time Complexity (Average Case): O(1)
+   */
   public set(key: string, value: T): void {
     const keyHash = hash(key, this.map.length)
     this.map[keyHash].push({
@@ -27,9 +32,30 @@ export class HashTable<T> implements HashTableInterface<T> {
     })
   }
 
+  /**
+   * Time Complexity (Average Case): O(1)
+   */
   public get(key: string): T | null {
     const keyHash = hash(key, this.map.length)
     const bucket = this.map[keyHash]
     return bucket.find((item) => item.key === key)?.value || null
+  }
+
+  /**
+   * Time Complexity: O(n)
+   */
+  public keys(): string[] {
+    const keys = new Set()
+    this.map.map((bucket) => bucket.map((item) => keys.add(item.key)))
+    return Array.from(keys)
+  }
+
+  /**
+   * Time Complexity: O(n)
+   */
+  public values(): T[] {
+    const values = new Set()
+    this.map.map((bucket) => bucket.map((item) => values.add(item.value)))
+    return Array.from(values)
   }
 }
