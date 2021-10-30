728913b0bdd6a616666c367cee37a6edbae4221b
Arslanoov <rasularslanoov@gmail.com>
2021-10-30 14:31:39 +0500

    feat(graph): Remove vertex and edge

--- src/data-structures/graph/graph.ts
+++ src/data-structures/graph/graph.ts
@@ -1,6 +1,8 @@
 export interface GraphInterface {
   addVertex(key: string): void
   addEdge(vertex1: string, vertex2: string): void
+  removeEdge(vertex1: string, vertex2: string): void
+  removeVertex(vertex: string): void
 }
 
 type List<T> = {
@@ -10,14 +12,28 @@ type List<T> = {
 export class Graph<T> implements GraphInterface {
   public list: List<T> = {}
 
-  public addVertex(vertex: string) {
+  public addVertex(vertex: string): void {
     if (!this.list[vertex]) {
       this.list[vertex] = []
     }
   }
 
-  public addEdge(vertex1: string, vertex2: string) {
+  public addEdge(vertex1: string, vertex2: string): void {
     this.list[vertex1].push(vertex2)
     this.list[vertex2].push(vertex1)
+  }
+
+  public removeEdge(vertex1: string, vertex2: string): void {
+    this.list[vertex1] = this.list[vertex1].filter((item) => item !== vertex2)
+    this.list[vertex2] = this.list[vertex2].filter((item) => item !== vertex1)
+  }
+
+  public removeVertex(vertex: string): void {
+    for (let i = 0; i < this.list[vertex].length; i++) {
+      const lastVertex = this.list[vertex].pop()
+      this.removeEdge(vertex, lastVertex)
+    }
+
+    delete this.list[vertex]
   }
 }
