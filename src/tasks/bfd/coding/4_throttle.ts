a98e77227f9b80d2715c7f7d79874b2aa63cc7ee
Arslanoov <rasularslanoov@gmail.com>
2021-11-25 12:28:26 +0500

    feat(tasks): BFD throttle

---
+++ src/tasks/bfd/coding/4_throttle.ts
@@ -0,0 +1,23 @@
+
+/**
+ * @param {Function} func
+ * @param {number} wait
+ */
+function throttle(func, wait) {
+  let isHappening = true
+  let args = null
+
+  return function (...params) {
+    if (isHappening) {
+      isHappening = false
+      func.apply(this, params)
+
+      window.setTimeout(() => {
+        isHappening = true
+        args && func.apply(this, args)
+      }, wait)
+    } else {
+      args = [...params]
+    }
+  }
+}
