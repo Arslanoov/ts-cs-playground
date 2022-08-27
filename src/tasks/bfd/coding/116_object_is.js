6f5e2cb6ca10fd4a432d4d48d33f5eaf1c8629e8
Расуль Арсланов <rasularslanoov@gmail.com>
2022-08-27 19:54:30 +0500

    feat(tasks): BFD Object is

---
+++ src/tasks/bfd/coding/116_object_is.js
@@ -0,0 +1,20 @@
+/**
+ * @param {any} a
+ * @param {any} b
+ * @return {boolean}
+ */
+function is(a, b) {
+    if (Number.isNaN(a) && Number.isNaN(b)) {
+        return true;
+    }
+
+    if (
+        a === 0 &&
+        b === 0 &&
+        1 / a !== 1 / b
+    ) {
+        return false;
+    }
+
+    return a === b;
+}
