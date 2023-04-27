eb811ebcb0ab2e06d43d3d395057a9f100c0d72c
Rasul <rasularslanoov@gmail.com>
2023-04-28 01:20:01 +0500

    feat(tasks): BFD Immutable helper

---
+++ src/tasks/bfd/coding/12_immutable_helper.js
@@ -0,0 +1,41 @@
+
+
+/**
+ * @param {any} data
+ * @param {Object} command
+ */
+function update(data, command) {
+    for (const [key, value] of Object.entries(command)) {
+        switch (key) {
+            case '$push':
+                return [...data, ...value];
+
+            case '$set':
+                return value;
+
+            case '$merge':
+                if (!(data instanceof Object)) {
+                    throw new Error();
+                }
+
+
+                return {...data, ...value};
+
+            case '$apply':
+                return value(data);
+
+            default:
+                if (Array.isArray(data)) {
+                    const result = [...data];
+                    result[key] = update(data[key], value);
+                    return result;
+                } else {
+                    return {
+                        ...data,
+                        [key]: update(data[key], value)
+                    };
+                }
+        }
+    }
+}
+
